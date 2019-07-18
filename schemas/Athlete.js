d345885041bf857d1b6c8589a3461247f881e1da
ajm4zs <alex_mulch@hotmail.com>
2019-07-18 17:52:44 -0400

    added xp and education functions to athlete schema

--- schemas/Athlete.js
+++ schemas/Athlete.js
@@ -134,6 +134,41 @@ AthleteSchema.statics.addExperienceById = function (id, experience, callback) {
 	);
 };
 
+AthleteSchema.statics.updateExperienceById = function (id, experience, callback) {
+
+	this.findById(id, function (error, athlete) {
+		if (error) return void callback(error);
+		if (!athlete) return void callback(new errors.NotFoundError('Athlete not found.'));
+
+		const e = athlete.experiences.id(experience._id);
+		if (!e) return void callback(new errors.NotFoundError('Experience is not found.'));
+		e.update(experience);
+
+		athlete.save(function (error, athlete) {
+			if (error) return void callback(error);
+			if (!athlete) return void callback(new errors.NotFoundError('Athlete not found.'));
+			callback(null, athlete.experiences);
+		});
+	});
+};
+
+AthleteSchema.statics.deleteExperienceById = function (id, experienceId, callback) {
+	this.findById(id, function (error, athlete) {
+		if (error) return void callback(error);
+		if (!athlete) return void callback(new errors.NotFoundError('Athlete not found.'));
+
+		const e = athlete.experiences.id(experienceId);
+		if (!e) return void callback(null, athlete.experiences);
+		e.remove();
+
+		athlete.save(function (error, athlete) {
+			if (error) return void callback(error);
+			if (!athlete) return void callback(new errors.NotFoundError('Athlete not found.'));
+			callback(null, athlete.experiences);
+		});
+	});
+};
+
 AthleteSchema.statics.addEducationById = function (id, education, callback) {
 
 	this.findByIdAndUpdate(
@@ -146,6 +181,41 @@ AthleteSchema.statics.addEducationById = function (id, education, callback) {
 			callback(null, athlete.education);
 		}
 	);
+};
+
+AthleteSchema.statics.updateEducationById = function (id, education, callback) {
+
+	this.findById(id, function (error, athlete) {
+		if (error) return void callback(error);
+		if (!athlete) return void callback(new errors.NotFoundError('Athlete not found.'));
+
+		const e = athlete.educaiton.id(education._id);
+		if (!e) return void callback(new errors.NotFoundError('Education is not found.'));
+		e.update(education);
+
+		athlete.save(function (error, athlete) {
+			if (error) return void callback(error);
+			if (!athlete) return void callback(new errors.NotFoundError('Athlete not found.'));
+			callback(null, athlete.education);
+		});
+	});
+};
+
+AthleteSchema.statics.deleteEducationById = function (id, educationId, callback) {
+	this.findById(id, function (error, athlete) {
+		if (error) return void callback(error);
+		if (!athlete) return void callback(new errors.NotFoundError('Athlete not found.'));
+
+		const e = athlete.education.id(educationId);
+		if (!e) return void callback(null, athlete.educatio);
+		e.remove();
+
+		athlete.save(function (error, athlete) {
+			if (error) return void callback(error);
+			if (!athlete) return void callback(new errors.NotFoundError('Athlete not found.'));
+			callback(null, athlete.education);
+		});
+	});
 };
 
 module.exports = AthleteSchema;
