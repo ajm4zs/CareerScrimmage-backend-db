62fa70a64c9a41918f61b020c4ae668c2530b51e
ajm4zs <alex_mulch@hotmail.com>
2019-07-21 15:37:26 -0400

    changed opportunity schema in Employer schema

--- schemas/Employer.js
+++ schemas/Employer.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const OpportunitySchema = require('./Opportunity');
+const errors = require('careerscrimmage-backend-utils').errors;
 
 const EmployerSchema = new Schema({
 	type: {
@@ -13,7 +15,56 @@ const EmployerSchema = new Schema({
 	industry: {
 		type: String
 	},
-	opportunities: [{ type: Schema.ObjectId, ref: 'Opportunity' }]
+	opportunities: [OpportunitySchema]
 }, { timestamps: true });
+
+EmployerSchema.statics.addOpportunityById = function (id, opportunity, callback) {
+
+	this.findByIdAndUpdate(
+		id,
+		{ $addToSet: { opportunities: opportunity } },
+		{ safe: true, new: true },
+		function (error, employer) {
+			if (error) return void callback(error);
+			if (!employer) return void callback(new errors.NotFoundError('Employer not found'));
+			callback(null, employer.opportunities);
+		}
+	);
+};
+
+EmployerSchema.statics.updateOpportunityById = function (id, opportunity, callback) {
+
+	this.findById(id, function (error, employer) {
+		if (error) return void callback(error);
+		if (!employer) return void callback(new errors.NotFoundError('Employer not found.'));
+
+		const o = employer.opportunities.id(opportunity._id);
+		if (!o) return void callback(new errors.NotFoundError('Opportunity is not found.'));
+		o.update(opportunity);
+
+		employer.save(function (error, employer) {
+			if (error) return void callback(error);
+			if (!employer) return void callback(new errors.NotFoundError('Employer not found.'));
+			callback(null, employer.opportunities);
+		});
+	});
+};
+
+EmployerSchema.statics.deleteOpportunityById = function (id, opportunityId, callback) {
+	this.findById(id, function (error, employer) {
+		if (error) return void callback(error);
+		if (!employer) return void callback(new errors.NotFoundError('Employer not found.'));
+
+		const o = employer.opportunities.id(opportunityId);
+		if (!o) return void callback(null, employer.opportunities);
+		o.remove();
+
+		employer.save(function (error, employer) {
+			if (error) return void callback(error);
+			if (!employer) return void callback(new errors.NotFoundError('Employer not found.'));
+			callback(null, employer.opportunities);
+		});
+	});
+};
 
 module.exports = EmployerSchema;
