c6af0cbaa475d38dd881c65ffb7c7cc38637b5ac
ajm4zs <alex_mulch@hotmail.com>
2019-09-09 18:18:08 -0500

    added addApplicationById method to opportunity schema

--- schemas/Opportunity.js
+++ schemas/Opportunity.js
@@ -6,6 +6,7 @@ const async = require('async');
 const bcrypt = require('bcrypt');
 
 const utilities = require('./../lib/utilities');
+const errors = require('careerscrimmage-backend-utils').errors;
 
 const OpportunitySchema = new Schema({
 	name: {
@@ -57,6 +58,20 @@ OpportunitySchema.methods.update = function (updated) {
 	this.startDate = updated.startDate;
 	this.endDate = updated.endDate;
 	this.deadlineDate = updated.deadlineDate;
+};
+
+OpportunitySchema.statics.addApplicationById = function (id, application, callback) {
+
+	this.findByIdAndUpdate(
+		id,
+		{ $addToSet: { applications: application } },
+		{ safe: true, new: true },
+		function (error, opportunity) {
+			if (error) return void callback(error);
+			if (!opportunity) return void callback(new errors.NotFoundError('Opportunity not found'));
+			callback(null, opportunity.applications);
+		}
+	);
 };
 
 module.exports = OpportunitySchema;
